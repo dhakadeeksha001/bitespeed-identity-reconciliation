d7a88ad5bd1ebebeb5408a671a49a80094204947
Deeksha Dhakad <dhakadeeksha001@gmail.com>
2025-05-27 15:22:54 +0530

    Database updates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const identifyRoute = require('./routes/identify');
@@ -7,10 +8,18 @@ const identifyRoute = require('./routes/identify');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
+app.get('/', (req, res) => {
+    res.send('Identity Reconciliation API is running.');
+});
+
 app.use('/identify', identifyRoute);
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Route not found' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
