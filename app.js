3264a3d839aae5f5fd548c8509dd4d4bc27a740e
Deeksha Dhakad <dhakadeeksha001@gmail.com>
2025-05-27 16:26:43 +0530

    Update files and remove node_modules using .gitignore

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 require('dotenv').config();
 
 const identifyRoute = require('./routes/identify');
@@ -8,18 +7,10 @@ const identifyRoute = require('./routes/identify');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Identity Reconciliation API is running.');
-});
-
+// Routes
 app.use('/identify', identifyRoute);
-
-app.use((req, res) => {
-    res.status(404).send({ message: 'Route not found' });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
